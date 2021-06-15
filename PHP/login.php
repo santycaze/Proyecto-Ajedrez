14c7e05bf26aab198193602cd04c66de701ec95e
ManuelAlonso1 <manuelalonsodesign@gmail.com>
2021-06-15 18:09:46 -0400

    b

---
+++ PHP/login.php
@@ -0,0 +1,21 @@
+<?php
+    $username = $_POST['user'];  
+    $password = $_POST['pass'];  
+       
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "select *from login where username = '$username' and password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          //valida si se loguea o no
+        if($count == 1){  
+            echo "<h1><center> Logeado! </center></h1>";  
+        }  
+        else{  
+            echo "<h1> Usuario o contraseña invalido </h1>";  
+        }     
+?>
