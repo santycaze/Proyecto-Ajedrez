d6429326f4205f1395926a94381cf62b8a37018a
Firpito <medicenfirpito@gmail.com>
2021-07-07 20:30:20 -0300

    MEJORA DE TABLERO Y TABLAS DEL MODULO ADMINISTRADOR

--- JS/administrador.js
+++ JS/administrador.js
@@ -8,10 +8,17 @@ function TraeUsuarios() {
       for (let i = 0; i < Datos.length; i++) {
         console.log(Datos[i]);
         var tablaAprobados = document.getElementById("tablaAprobados");
+        var tablaNoAprobados = document.getElementById("tablaNoAprobados");
+        /*
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------
+        */
         if(Datos[i].Aprobado == 1){
-          contenido = "<tr><td><div class='td1'>"+Datos[i].nombreUsuario+"</td></div><td><div class='td1'>IEP</div></td><td><div class='td1'>Periodista</div></td></tr>";
+          contenido = "<tr><td><div class='td1'>"+Datos[i].nombreUsuario+"</td></div><td><div class='td1'>IEP</div></td><td><div class='td1'>Periodista</div></td><td><div class='td1'><button>✖</button></div></td></tr>";
+          $(tablaAprobados).append(contenido);
+        }else{
+          contenido = "<tr><td><div class='td1'>"+Datos[i].nombreUsuario+"</td></div><td><div class='td1'>IEP</div></td><td><div class='td1'>Periodista</div></td><td><div class='td1'><button>✓</button><button>✖</button></div></td></tr>";
+          $(tablaNoAprobados).append(contenido);
         }
-        $(tablaAprobados).append(contenido);
       }
     }
   });
