6e2fa5709f0aace474778c7935983246f521ba0c
ManuelAlonso1 <manuelalonsodesign@gmail.com>
2021-07-12 18:07:36 -0300

    asd

--- JS/administrador.js
+++ JS/administrador.js
@@ -25,6 +25,58 @@ function TraePeriodistas() {
 
 }
 
+function TraeUsuarios() {
+  $.ajax({
+    type: "POST",
+    url: "../PHP/TraePeriodistas.php",
+    success: function (response) {
+      var Datos = JSON.parse(response);
+      for (let i = 0; i < Datos.length; i++) {
+        console.log(Datos[i]);
+        var tablaAprobados = document.getElementById("tablaAprobados");
+        var tablaNoAprobados = document.getElementById("tablaNoAprobados");
+        /*
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------
+        */
+        if(Datos[i].Aprobado == 1){
+          contenido = "<tr><td><div class='td1'>"+Datos[i].nombreUsuario+"</td></div><td><div class='td1'>IEP</div></td><td><div class='td1'>Periodista</div></td><td><div class='td1'><button>✖</button></div></td></tr>";
+          $(tablaAprobados).append(contenido);
+        }else{
+          contenido = "<tr><td><div class='td1'>"+Datos[i].nombreUsuario+"</td></div><td><div class='td1'>IEP</div></td><td><div class='td1'>Periodista</div></td><td><div class='td1'><button>✓</button><button>✖</button></div></td></tr>";
+          $(tablaNoAprobados).append(contenido);
+        }
+      }
+    }
+  });
+
+}
+
+function TraeContraseñas() {
+  $.ajax({
+    type: "POST",
+    url: "../PHP/TraePeriodistas.php",
+    success: function (response) {
+      var Datos = JSON.parse(response);
+      for (let i = 0; i < Datos.length; i++) {
+        console.log(Datos[i]);
+        var tablaAprobados = document.getElementById("tablaAprobados");
+        var tablaNoAprobados = document.getElementById("tablaNoAprobados");
+        /*
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------
+        */
+        if(Datos[i].Aprobado == 1){
+          contenido = "<tr><td><div class='td1'>"+Datos[i].nombreUsuario+"</td></div><td><div class='td1'>IEP</div></td><td><div class='td1'>Periodista</div></td><td><div class='td1'><button>✖</button></div></td></tr>";
+          $(tablaAprobados).append(contenido);
+        }else{
+          contenido = "<tr><td><div class='td1'>"+Datos[i].nombreUsuario+"</td></div><td><div class='td1'>IEP</div></td><td><div class='td1'>Periodista</div></td><td><div class='td1'><button>✓</button><button>✖</button></div></td></tr>";
+          $(tablaNoAprobados).append(contenido);
+        }
+      }
+    }
+  });
+
+}
+
 function Periodistas() {
   var tabla1 = "<table id='tablaNoAprobados'> <h2>Pendientes de aprobar</h2><tr> <th id='col1'>Nombre</th> <th id='col1'>Institucion</th>  <th id='col1'>Tipo</th>  <th id='col1'>A/R</th></tr></table>";
   $("#tabla1").html(tabla1);
