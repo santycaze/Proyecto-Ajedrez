7bc94ff56c703bae6b26e474caf8245e1d7ad270
ManuelAlonso1 <manuelalonsodesign@gmail.com>
2021-07-16 02:43:27 -0300

    Aasd

---
+++ Usuario/PHP/VerPerfil.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../CSS/VerPerfil.css">
+    <script src="https://kit.fontawesome.com/1e193e3a23.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<div class="perfil-wrapper">
+    <div class="perfil">
+        <div class="contenedor-wrapper">
+            <div class="foto-perfil-verperfil">
+                <img src="../../IMG/icono2.png" alt=""> <h1>Socrates</h1>
+            </div>
+            <div class="contenedor-nombres-trofeos-verperfil">
+                <div class="contenedor-trofeos-verperfil">
+                    <h2>Trofeos</h2>
+                    <div class="trofeos-wrapper">
+                        <div class="trofeo-verperfil"><h2>Trofeo1</h2><div class="trofeo-icono-verperfil"><i class="fas fa-trophy"></i></div></div>
+                        <div class="trofeo-verperfil"><h2>Trofeo2</h2><div class="trofeo-icono-verperfil"><i class="fas fa-trophy"></i></div></div>
+                        <div class="trofeo-verperfil"><h2>Trofeo3</h2><div class="trofeo-icono-verperfil"><i class="fas fa-trophy"></i></div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="contenedor-logros-verperfil">
+            <div class="icono-logro-verperfil"> <i class="fas fa-medal"></i> </div>
+            <div class="icono-logro-verperfil"><i class="fas fa-medal"></i></div>
+            <div class="icono-logro-verperfil"><i class="fas fa-medal"></i></div>
+            <div class="icono-logro-verperfil"><i class="fas fa-medal"></i></div>
+            <div class="icono-logro-verperfil"><i class="fas fa-medal"></i></div>
+            <div class="icono-logro-verperfil"><i class="fas fa-medal"></i></div>
+            <div class="icono-logro-verperfil"><i class="fas fa-medal"></i></div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
