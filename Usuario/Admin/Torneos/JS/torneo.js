bc4487bdcca66fc6fa3e78478e6439534993f3aa
Firpito <medicenfirpito@gmail.com>
2021-07-19 16:41:02 -0300

    ...

--- Usuario/Admin/Torneos/JS/torneo.js
+++ Usuario/Admin/Torneos/JS/torneo.js
@@ -1,6 +1,9 @@
 function Torneo() {
-    var tabla1 = '<input type="text" placeholder="Nombre del Torneo"> <input type="date" placeholder="Apertura de inscripciones"> <input type="date" placeholder="Fin de inscripciones"> <input type="date" placeholder="Comienzo del torneo">  <input type="date" placeholder="Fin del torneo"> <input type="number" placeholder="Max de participantes">';
+    var tabla1 = '<h2>Creacion de torneos</h2> <input type="text" placeholder="Nombre del Torneo"> <input type="date" placeholder="Apertura de inscripciones"> <input type="date" placeholder="Fin de inscripciones"> <input type="date" placeholder="Comienzo del torneo">  <input type="date" placeholder="Fin del torneo"> <input type="number" placeholder="Max de participantes"> <input type="number" placeholder="N.partidas por juego"> <input type="number" placeholder="Tiempo max por partida"> <input type="number" placeholder="Tiempo por movida"> <input type="number" placeholder="Max de participantes"> <input type="text" placeholder="Nombre del trofeo">';
     $("#tabla1").html(tabla1);
+    if () {
+        
+    }
     var tabla2 = "<table id='tablaAprobados'> <h2>Aprobados</h2><tr> <th id='col1'>Nombre</th> <th id='col1'>Institucion</th>  <th id='col1'>Tipo</th>  <th id='col1'>Inhabilitar</th></tr></table>";
     $("#tabla2").html(tabla2);
 }
