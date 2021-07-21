5ccefa9487af686d860ebde8c23ce62472df1e17
Luciano2244 <64085517+Luciano2244@users.noreply.github.com>
2021-07-21 11:46:13 -0300

    Update login.php

--- Usuario/PHP/login.php
+++ Usuario/PHP/login.php
@@ -1,6 +1,5 @@
 <?php
-    include 'servidor.php';
-    include 'conexion.php';
+    include '../../PHP/conexion.php';
 
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
@@ -9,12 +8,7 @@
     $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
     $nombreUsuario = $_POST['user'];  
-    $contraseña = $_POST['pass']; 
-        
-        $nombreUsuario = stripcslashes($nombreUsuario);  
-        $contraseña = stripcslashes($contraseña);  
-        $nombreUsuario = mysqli_real_escape_string($db, $nombreUsuario);  
-        $contraseña = mysqli_real_escape_string($db, $contraseña);
+    $contraseña = $_POST['pass'];   
       
         $sql = "select * from usuario where nombreUsuario = '$nombreUsuario' and contraseña = '$contraseña'";  
         $result = $mysqli->query($sql); 
