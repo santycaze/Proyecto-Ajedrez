99fe6a189fc9a86c04cbe11b591903f727f44841
Firpito <gastonfirpo78@gmail.com>
2021-07-22 10:21:50 -0300

    ...

--- Usuario/JS/login.js
+++ Usuario/JS/login.js
@@ -6,29 +6,46 @@ function iniciarSesion() {
         type: "POST",
         url: "../Usuario/PHP/login.php",
         data: { user: usr, pass: pass },
-        success: function(response) {
+        success: function (response) {
             console.log(response);
-           if (response != 1) {
-            sessionStorage.setItem("j1", response);
-               window.location = "../index.html";
-           }
+            if (response != 1) {
+                sessionStorage.setItem("j1", response);
+                window.location = "../index.html";
+            }
         }
     });
 }
-function sesion(usr) {
+
+function cerrarSesion() {
+    var usr = document.getElementById('usr').value;
+    var pass = document.getElementById('pass').value;
+
     $.ajax({
         type: "POST",
-        url: "/PHP/sesionPrueba.php",
-        data: {usr:usr},
+        url: "../Usuario/PHP/login.php",
+        data: { user: usr, pass: pass },
         success: function (response) {
-            
-            $("#elcosodellogin").html('<i id="foto"></i> <p id="nick"></p>')
-            $("#nick").html(sessionStorage.getItem("j1"))
-            $("#foto").html("<img id='foto2' src='../Proyecto-Ajedrez/IMG/Icono3.png'></img>")
+            console.log(response);
+            if (response != 1) {
+                sessionStorage.setItem("j1", response);
+                window.location = "../index.html";
+            }
         }
     });
 }
-/*
 
+function actualizarNick(usr) {
+    if (usr != null) {
+        $("#elcosodellogin").html('<i id="foto"></i> <p id="nick"></p>')
+        $("#elcosodellogin").attr('disabled','true')
+        $("#nick").html(sessionStorage.getItem("j1"))
+        $("#foto").html("<img id='foto2' src='../Proyecto-Ajedrez/IMG/Icono3.png'></img>")
+    }else{
+        <button onclick="window.location.href='../Proyecto-Ajedrez/HTML/login.html'" id="elcosodellogin">
+        <div id="iconoUsr"><i class="fas fa-user" id="foto"></i></div>
+        <p id="nick">Log in</p>
+        </button>
+    }
 
-*/
+}
+
