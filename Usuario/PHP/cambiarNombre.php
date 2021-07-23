52cb809b964b25bc8b6c95192e8e74a443c57ba9
Firpito <gastonfirpo78@gmail.com>
2021-07-23 10:05:55 -0300

    ...

--- Usuario/PHP/cambiarNombre.php
+++ Usuario/PHP/cambiarNombre.php
@@ -1,5 +1,7 @@
 <?php
      include "../../PHP/Servidor/servidor.php";
+     $nombreActual = $_POST['nombreActual'];
+     $nombreNuevo = $_POST['nuevoNombre'];
      $servidor = new Servidor();
-     $servidor->cambiarNombre();
+     $servidor->cambiarNombre($nombreActual,$nombreNuevo);
 ?>
