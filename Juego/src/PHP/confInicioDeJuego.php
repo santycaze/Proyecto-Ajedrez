709dcf86e61a67ff68043fc5502ce77491115567
Firpito <medicenfirpito@gmail.com>
2021-08-22 19:58:48 -0300

    ...

--- Juego/src/PHP/confInicioDeJuego.php
+++ Juego/src/PHP/confInicioDeJuego.php
@@ -1,17 +1,23 @@
 <?php
 include "instanciaDeJuego.php";
 
+$jugador = $_POST['jugadores'];
+
+$jugadores = [];
 $instancias = [];
 
-$ij1 = new instanciaDeJuego("1qfr3wef14cw","Firpo","Larry");
-$ij2 = new instanciaDeJuego("1qfrfasdgtrt","pulga","Larry");
-$ij3 = new instanciaDeJuego("1qfr32345dvs","Firpo","pulga");
-$ij4 = new instanciaDeJuego("1qfg45ed2ccw","Firpo","Usuario5");
-$ij5 = new instanciaDeJuego("1q!@wercwe34","Usuario3","Larry");
+array_push($jugadores,$jugador);
 
-array_push($instancias,$ij1->obtenerInstancia(),$ij2->obtenerInstancia(),$ij3->obtenerInstancia(),$ij4->obtenerInstancia(),$ij5->obtenerInstancia());
+if (!isset($jugadores[1])) {
+    $ij = new instanciaDeJuego("1qfr3wef14cw",$jugadores[0],null);
+}else {
+    $ij = new instanciaDeJuego("1qfr3wef14cw",$jugadores[0],$jugadores[1]);
+}
+
+
+array_push($instancias,$ij->obtenerInstancia());
 
 for ($i=0; $i < sizeof($instancias); $i++) { 
-    echo $instancias[$i]."\n";
+    echo $instancias[$i];
 }
 
