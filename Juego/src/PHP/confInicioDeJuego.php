5b60bff1b6350fd8d13dbd3b11f381541bb709d0
Firpito <medicenfirpito@gmail.com>
2021-08-21 23:05:10 -0300

    ...

--- Juego/src/PHP/confInicioDeJuego.php
+++ Juego/src/PHP/confInicioDeJuego.php
@@ -9,21 +9,6 @@ $ij3 = new instanciaDeJuego("1qfr32345dvs","Firpo","pulga");
 $ij4 = new instanciaDeJuego("1qfg45ed2ccw","Firpo","Usuario5");
 $ij5 = new instanciaDeJuego("1q!@wercwe34","Usuario3","Larry");
 
-$ij1->setColorJugador1();
-$ij1->setColorJugador2();
-
-$ij2->setColorJugador1();
-$ij2->setColorJugador2();
-
-$ij3->setColorJugador1();
-$ij3->setColorJugador2();
-
-$ij4->setColorJugador1();
-$ij4->setColorJugador2();
-
-$ij5->setColorJugador1();
-$ij5->setColorJugador2();
-
 array_push($instancias,$ij1->obtenerInstancia(),$ij2->obtenerInstancia(),$ij3->obtenerInstancia(),$ij4->obtenerInstancia(),$ij5->obtenerInstancia());
 
 for ($i=0; $i < sizeof($instancias); $i++) { 
--- Juego/src/PHP/instanciaDeJuego.php
+++ Juego/src/PHP/instanciaDeJuego.php
@@ -13,9 +13,10 @@ class instanciaDeJuego
         $this->jugador1 = $jugador1;
         $this->jugador2 = $jugador2;
     }
-
+    
     function obtenerInstancia(){
         if ($this->jugador2 == null) {
+            $this->setColorJugador1();
             $instanciaDeJuego = [
                 'idTablero' => $this->idTablero,
                 'jugador1' => $this->jugador1,
@@ -24,6 +25,8 @@ class instanciaDeJuego
                 'colorJugador2' => $this->colorJugador2
             ];
         }else{
+            $this->setColorJugador1();
+            $this->setColorJugador2();
             $instanciaDeJuego = [
                 'idTablero' => $this->idTablero,
                 'jugador1' => $this->jugador1,
@@ -35,7 +38,6 @@ class instanciaDeJuego
         return json_encode($instanciaDeJuego);
     }
 
-    
     function setColorJugador1(){
         $this->colorJugador1 = rand(0, 1);
     }
@@ -48,5 +50,4 @@ class instanciaDeJuego
         }
     }
 }
-
 ?>
