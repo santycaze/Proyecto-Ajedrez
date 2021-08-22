787f37b0f3d4f2c348a29caa62ff2ae16b2bc512
Firpito <gastonfirpo78@gmail.com>
2021-08-21 21:47:15 -0300

    instancia de juego

--- Juego/src/PHP/confInicioDeJuego.php
+++ Juego/src/PHP/confInicioDeJuego.php
@@ -1,9 +1,32 @@
 <?php
 include "instanciaDeJuego.php";
 
-$ij = new instanciaDeJuego("1qfr3wef14cw","Firpo","Larry");
+$instancias = [];
 
-$ij->setColorJugador1(1);
-$ij->setColorJugador2(0);
+$ij1 = new instanciaDeJuego("1qfr3wef14cw","Firpo","Larry");
+$ij2 = new instanciaDeJuego("1qfrfasdgtrt","pulga","Larry");
+$ij3 = new instanciaDeJuego("1qfr32345dvs","Firpo","pulga");
+$ij4 = new instanciaDeJuego("1qfg45ed2ccw","Firpo","Usuario5");
+$ij5 = new instanciaDeJuego("1q!@wercwe34","Usuario3","Larry");
 
-echo $ij->mostrarInstancia();
+$ij1->setColorJugador1();
+$ij1->setColorJugador2();
+
+$ij2->setColorJugador1();
+$ij2->setColorJugador2();
+
+$ij3->setColorJugador1();
+$ij3->setColorJugador2();
+
+$ij4->setColorJugador1();
+$ij4->setColorJugador2();
+
+$ij5->setColorJugador1();
+$ij5->setColorJugador2();
+
+array_push($instancias,$ij1->obtenerInstancia(),$ij2->obtenerInstancia(),$ij3->obtenerInstancia(),$ij4->obtenerInstancia(),$ij5->obtenerInstancia());
+
+for ($i=0; $i < sizeof($instancias); $i++) { 
+    echo $instancias[$i]."\n";
+}
+
--- Juego/src/PHP/instanciaDeJuego.php
+++ Juego/src/PHP/instanciaDeJuego.php
@@ -3,7 +3,9 @@ class instanciaDeJuego
 {
     private $idTablero;
     private $jugador1;
+    private $colorJugador1;
     private $jugador2;
+    private $colorJugador2;
     private $infoJugadores = [];
 
     function __construct($idTablero,$jugador1,$jugador2){
@@ -12,29 +14,38 @@ class instanciaDeJuego
         $this->jugador2 = $jugador2;
     }
 
-    function mostrarInstancia(){
-        if ($this->jugador1 == null || $this->jugador2 == null || $this->idTablero == null) {
-            echo "instancia de juego incompleta...";
-            $instanciaDeJuego = null;
+    function obtenerInstancia(){
+        if ($this->jugador2 == null) {
+            $instanciaDeJuego = [
+                'idTablero' => $this->idTablero,
+                'jugador1' => $this->jugador1,
+                'jugador2' => $this->jugador2,
+                'colorJugador1' => $this->colorJugador1,
+                'colorJugador2' => $this->colorJugador2
+            ];
         }else{
             $instanciaDeJuego = [
                 'idTablero' => $this->idTablero,
                 'jugador1' => $this->jugador1,
                 'jugador2' => $this->jugador2,
-                'colorJugador1' => $this->infoJugadores[$this->jugador1],
-                'colorJugador2' => $this->infoJugadores[$this->jugador2]
+                'colorJugador1' => $this->colorJugador1,
+                'colorJugador2' => $this->colorJugador2
             ];
         }
         return json_encode($instanciaDeJuego);
     }
 
     
-    function setColorJugador1($color){
-        $this->infoJugadores[$this->jugador1] = $color;
+    function setColorJugador1(){
+        $this->colorJugador1 = rand(0, 1);
     }
 
-    function setColorJugador2($color){
-        $this->infoJugadores[$this->jugador2] = $color;
+    function setColorJugador2(){
+        if ($this->colorJugador1 == 1 && $this->jugador2 != null) {
+            $this->colorJugador2 = 0;
+        }else if($this->jugador2 != null){
+            $this->colorJugador2 = 1;
+        }
     }
 }
 
