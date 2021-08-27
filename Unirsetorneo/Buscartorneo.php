b9b89479f6da7fae591cd2dc65d476ae5c0508f6
ManuelAlonso1 <manuelalonsodesign@gmail.com>
2021-08-26 22:56:29 -0400

    h

--- Unirsetorneo/Buscartorneo.php
+++ Unirsetorneo/Buscartorneo.php
@@ -2,4 +2,11 @@
      include "../PHP/Servidor/servidor.php";
      $servidor = new Servidor();
      echo json_encode($servidor->datosTorneo());
+     
+     $torneos = '
+     <div class="contenedor-torneo">
+                    <p id="Nombretorneo">Nombre torneo</p> <button>Ver</button>
+                </div>
+     ';
 ?>
+
