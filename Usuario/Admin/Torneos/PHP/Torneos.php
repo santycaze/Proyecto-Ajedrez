25e9220e5c61c6e7f4a5d1172b22582e3091cea4
Firpito <gastonfirpo78@gmail.com>
2021-09-01 15:33:28 -0300

    ...

---
+++ Usuario/Admin/Torneos/PHP/Torneos.php
@@ -0,0 +1,37 @@
+<?php
+
+include "CrearTorneo.php";
+include "Fixture.php";
+
+/*
+ * Parametros - crearTorneos
+ * 
+ * nombreTorneo - codigoIngreso - puntuacion - FCInscripciones - FFInscripciones - FCTorneo - FFTorneo - tiempoPartida - maximoParticipantes - tiempoMaxPartida - cantPartidas - horarios - tiempoMovimiento - nombreTrofeo - numeroPartidas
+ * 
+ */
+
+$NombreTorneo = $_POST['nombreTorneo'];
+$CodigoIngreso = $_POST['codIngreso'];
+$puntuacion = $_POST['puntuacion'];
+$FCInscripciones = $_POST['fci'];
+$FFInscripciones = $_POST['ffi'];
+$FCTorneo = $_POST['fct'];
+$FFTorneo = $_POST['fft'];
+$tiempoPartida = $_POST['timepoPartida'];
+$maximoParticipantes = $_POST['maxPart'];
+$timepoMaxPartida = $_POST['tiempoMaxPartida'];
+$cantPartidas = $_POST['cantPartida'];
+$horarios = $_POST['horarios'];
+$tiempoMovimiento = $_POST['tiempoMov'];
+$nombreTrofeo = $_POST['nombreTrofeo'];
+$numeroPartidas = $_POST['numeroParidas'];
+
+
+$torneos = new CrearTorneo($numeroTorneo,$CodigoIngreso,$puntuacion, $FCInscripciones,$FFInscripciones,$FCTorneo, $FFTorneo,$tiempoPartida,$maximoParticipantes,$timepoMaxPartida,$cantPartidas,$horarios,$tiempoMovimiento,$nombreTrofeo,$numeroPartidas);
+
+
+/**
+ * realizo validacion de datos antes de guardar el torneo;
+ */
+$torneos->devolverTorneo();
+?>
