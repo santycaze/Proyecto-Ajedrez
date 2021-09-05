7d699b7187cec2b9dd83e24f8d25777012703cf6
Firpito <gastonfirpo78@gmail.com>
2021-09-05 16:01:54 -0300

    ...

--- Juego/confInicioJuego.js
+++ Juego/confInicioJuego.js
@@ -1,4 +1,4 @@
-const socket = io('http://192.168.1.5:3000');
+const socket = io('http://localhost:3000');
 
 var datosJuego;
 var posicionPieza = 7;
--- Juego/server.js
+++ Juego/server.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(3000, {
     cors: {
-        origin: "http://192.168.1.5",
+        origin: "http://localhost",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
