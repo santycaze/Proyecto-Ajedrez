42391d6172e9369b9e46f44222ecf29e731725f1
santycaze <64055722+santycaze@users.noreply.github.com>
2021-09-08 12:22:32 -0300

    ...

---
+++ Usuario/PHP/contacto.php
@@ -0,0 +1,22 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: $name'; 
+    $to = 'santycaze@gmail.com'; 
+    $subject = 'Soporte Técnico - 8BitTech';
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $select_enquirytype = strip_tags($_POST['enquirytype']);
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message  \n IP User: $ip";
+?>
+
+<?php
+if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Gracias, nos comunicaremos lo mas pronto posible!</p>';
+    } else { 
+        echo '<p>Oh no! Hubo un error, Intenta mas tarde :(</p>'; 
+    }
+}
+?>
