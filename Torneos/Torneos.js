a9997d5b792c4be7e7c47b1267459459b711893a
Firpito <gastonfirpo78@gmail.com>
2021-09-13 21:30:30 -0300

    ...

---
+++ Torneos/Torneos.js
@@ -0,0 +1,14 @@
+class Torneos {
+    constructor(usuario, Nombre, Apellido, mail, ci, celular, Fnacimiento, tipo, icono) {
+        this.Usuario = usuario;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Mail = mail;
+        this.Ci = ci;
+        this.Cel = celular;
+        this.Fnac = Fnacimiento;
+        this.Tipo = tipo;
+        this.Icono = icono;
+    }
+
+}
