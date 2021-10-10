4d4c80b04c025defd62c725ceb419a023ee75bc1
ManuelAlonso1 <manuelalonsodesign@gmail.com>
2021-10-10 18:22:20 -0300

    Fuchibol

--- index.php
+++ index.php
@@ -108,7 +108,7 @@ require_once 'Usuario/PHP/Sesion/logeado.php';
 
     <!-- Hero -->
     <div class=" contenedor-inicio">
-        <img src="IMG/logo.png" alt="" class="imagen-inicio">
+        <img src="./IMG/logo8bit.png" alt="" class="imagen-inicio">
         <h1>Juega Ajedrez Online</h1>
         <h2>8-Bit Chess</h2>
         <div class="contenedor-botones-inicio">
@@ -124,23 +124,15 @@ require_once 'Usuario/PHP/Sesion/logeado.php';
             }
             ?>
             <button class="boton-estadisticas">
-                <p>Estadisticas</p><i class="fas fa-chart-bar"></i>
+                <p>Entrenar</p><i class="fas fa-dumbbell"></i>
             </button>
         </div>
     </div>
     <!-- Fin Hero -->
 
     <!-- SVG -->
-    <svg height="150px" width="100%" id="svg" viewBox="0 0 1440 400" preserveAspectRatio=none
-        xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150">
-        <path
-            d="M 0,400 C 0,400 0,133 0,133 C 68.70813397129191,134.31100478468898 137.41626794258383,135.62200956937798 247,133 C 356.58373205741617,130.37799043062202 507.0430622009568,123.82296650717704 621,124 C 734.9569377990432,124.17703349282296 812.4114832535886,131.08612440191388 889,143 C 965.5885167464114,154.91387559808612 1041.311004784689,171.8325358851675 1133,171 C 1224.688995215311,170.1674641148325 1332.3444976076555,151.58373205741626 1440,133 C 1440,133 1440,400 1440,400 Z"
-            stroke="none" stroke-width="0" fill="#07375088" class="transition-all duration-300 ease-in-out delay-150">
-        </path>
-        <path
-            d="M 0,400 C 0,400 0,266 0,266 C 73.82775119617222,279.87559808612446 147.65550239234443,293.75119617224885 259,297 C 370.34449760765557,300.24880382775115 519.2057416267944,292.87081339712915 620,294 C 720.7942583732056,295.12918660287085 773.5215311004783,304.7655502392344 867,302 C 960.4784688995217,299.2344497607656 1094.708133971292,284.066985645933 1197,276 C 1299.291866028708,267.933014354067 1369.645933014354,266.96650717703346 1440,266 C 1440,266 1440,400 1440,400 Z"
-            stroke="none" stroke-width="0" fill="#073750ff" class="transition-all duration-300 ease-in-out delay-150">
-        </path>
+  
+       
     </svg>
     <!-- Fin SVG -->
 
@@ -192,17 +184,6 @@ require_once 'Usuario/PHP/Sesion/logeado.php';
 
 
     <!-- SVG -->
-    <svg height="150px" width="100%" id="svg" viewBox="0 0 1440 400" preserveAspectRatio=none
-        xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150">
-        <path
-            d="M 0,400 C 0,400 0,133 0,133 C 123.31100478468898,126.36842105263158 246.62200956937795,119.73684210526315 334,120 C 421.37799043062205,120.26315789473685 472.82296650717706,127.42105263157893 547,125 C 621.1770334928229,122.57894736842107 718.086124401914,110.57894736842105 836,100 C 953.913875598086,89.42105263157895 1092.8325358851673,80.26315789473685 1197,86 C 1301.1674641148327,91.73684210526315 1370.5837320574165,112.36842105263158 1440,133 C 1440,133 1440,400 1440,400 Z"
-            stroke="none" stroke-width="0" fill="#07375088" class="transition-all duration-300 ease-in-out delay-150"
-            transform="rotate(-180 720 200)" id="path"></path>
-        <path
-            d="M 0,400 C 0,400 0,266 0,266 C 90.00956937799043,262.9856459330143 180.01913875598086,259.9712918660287 263,260 C 345.98086124401914,260.0287081339713 421.93301435406704,263.1004784688995 533,253 C 644.066985645933,242.8995215311005 790.2488038277512,219.62679425837325 905,226 C 1019.7511961722488,232.37320574162675 1103.0717703349283,268.39234449760767 1187,280 C 1270.9282296650717,291.60765550239233 1355.4641148325359,278.8038277511962 1440,266 C 1440,266 1440,400 1440,400 Z"
-            stroke="none" stroke-width="0" fill="#073750ff" class="transition-all duration-300 ease-in-out delay-150"
-            transform="rotate(-180 720 200)" id="path"></path>
-    </svg>
     <!-- Fin SVG -->
 
     <!-- Opciones jugador -->
@@ -212,22 +193,16 @@ require_once 'Usuario/PHP/Sesion/logeado.php';
 
             <img src="IMG/OjoDePez.png" alt="">
             <div class="contenedor-opciones-juego">
-                <a class="boton-salas"> Opciones de juego <i class="fas fa-angle-down"></i></a>
-                <div class="opciones-juego-menu">
-                    <a class="a3" href="Unirsetorneo/Buscartorneo.html">Buscar torneo</a>
-                    <a class="a3" href="Unirsetorneo/VerTorneos.html">Ver partidos</a>
-                </div>
+                <a href="Unirsetorneo/Buscartorneo.html" class="boton-salas" > Buscar torneo </a>
+
             </div>
         </div>
 
         <div class="contenedor-entrenamiento">
             <img src="IMG/tutorial.png" alt="">
             <div class="contenedor-entrenamiento-menu">
-                <a class="boton-salas"> Aprende a jugar <i class="fas fa-angle-down"></i></a>
-                <div class="opciones-entrenamiento-menu">
-                    <a href="https://www.youtube.com/watch?v=x4R2EPkYzZk" class="a4">Ver tutorial</a>
-                    <a class="a4">Aprender a jugar</a>
-                </div>
+                <a href="https://www.youtube.com/watch?v=x4R2EPkYzZk" class="boton-salas"> Ver tutorial</a>
+                
             </div>
         </div>
 
@@ -241,11 +216,6 @@ require_once 'Usuario/PHP/Sesion/logeado.php';
 
 
     <!-- SVG -->
-    <div style="height: 150px; overflow: hidden;" id="svg"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
-            style="height: 100%; width: 100%;">
-            <path d="M0.00,49.98 C115.97,165.28 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
-                style="stroke: none; fill: #073750;" id="path"></path>
-        </svg></div>
     <!-- Fin SVG -->
 
     <!-- Seccion noticias -->
@@ -274,63 +244,12 @@ require_once 'Usuario/PHP/Sesion/logeado.php';
 </body>
 
 <!-- SVG -->
-<div style="height: 90px; overflow: hidden; background: #073750;" id="svg"><svg viewBox="0 0 500 150"
-        preserveAspectRatio="none" style="height: 100%; width: 100%;">
-        <path d="M0.00,49.98 C115.97,165.28 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
-            style="stroke: none; fill: #173248;" id="path"></path>
-    </svg></div>
 <!-- Fin SVG -->
 
 <!-- Footer -->
 <footer>
-    <div class="logo">
-        <img src="IMG/logo.png" alt="">
-    </div>
-    <div class="sobre-nosotros">
-        <h1>Sobre nosotros</h1>
-        <p> Somos una empresa que se dedica a proporcionar juegos de calidad orientados a la temática de 8 bits , con el
-            fin de que sean juegos competitivos pero a la misma vez divertidos para que nuestros clientes puedan revivir
-            sus mejores momentos de juego. </p>
-    </div>
-    <div class="menu">
-        <h1>Menu</h1>
-        <ul>
-            <li><a href="./HTML/sobrenosotros.html">Sobre Nosotros</a></li>
-            <li><a href="">Contacto</a></li>
-            <li><a href="">Jugar</a></li>
-        </ul>
-    </div>
+   <p>Copyright © 2021 8Bit-Tech. All rights reserved.</p>
 </footer>
 <!-- Fin Footer -->
 
 </html>
-
-<!--
-                           -++-   -+-.==:               .-=:   -#-.:-:
-                       :=-+****+: :=: :-.           :-.-=====- :-: ::.
-                     -***********+-               .-====++=-===:
-             .--   -*###*********+++-   ::.::   .========---==--=:   .:
-           :+#.:*-#**##***+****+=+++++-  -+-.=:===========-=-------:   .:
-            .+##=:*####****+++=-=+*+++++: -+++.-===========----------:   =-.
-             --:   :**####=::...::-+**++++--:-= .-==-        .-------=-: .=#
-         :--##=.    :###+:          -+*+++++- .::--            :----=-===:  --
-       .+%%%%%%%+.:*##*:              -+++++++:--                :---------: .=-
-     .+%%%%%%%%%%%%#*:     .:::::.      -+++++-       .-==-.       :--------.   -:
-   .=-+%%%%%%%%%%%*:      =##+++++=.      -++-       -======-.       :----.       ..
- .=: -#%%%%%%%#%*:     .=######**#*+=:      -++:  .-+++++=====-.       :+=:    :-:  ..
-   -#%%%#%%%%%*:     .+##=-########***+:      -++=+*###++=======-.       =#=.:+*---:
-   :*%%%%#*#%%#-      -##**######**#****+:      -+++#%#++========-       =#+-===---:
-    ::*%%%#%%%%%#-      -############=.-**+:      :++#*+++=====-       :#%#*+=---. . ..
-    .=-:*%%%%%%%###-      -########=     -**+:      :++++++==-       :-+##+===-. ...:.
-       =-:*%%%%%%##%*-      ::=-+=      .+****+:      :++++-       :====++==-. ....
-        .=-:*%%%#%##%#*-              .+##******+:               :======*+-. .:.:
-          .+-:*%%%#%%##*-.          .+#####*******+:           :========-:  ..:
-         -#%#===*%%%%##*-+=...:.---+#*####===+******+:.......:=+=======:   .:
-         .+##=.=-:*######*======-*#####**#=.++:=********+++++--+++===.   .:
-          -- :--.=-:++####***#=.  :*#*:-*..==+*-.=*******++-.  .-+=.     .::
-        -- -=.  --.=-:*%%%**++:     .-####+ =##---.=******+:        .==- :==:
-       -  .=.   .=. .=-:*%%%##%*.   -*######=.+:  --.=*****+=.    .=++===:
-            :-:-:      . :*%%##*:     -*####*:      : .=****+:     .=++=:
-                           :++.         :-              .==:         .:
-
--->
