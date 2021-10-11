5e2f31d7d327a3f03feb880f8279f812556904f6
Firpito <medicenfirpito@gmail.com>
2021-10-11 20:28:04 -0300

    ...

--- Unirsetorneo/Buscartorneo.php
+++ Unirsetorneo/Buscartorneo.php
@@ -8,7 +8,7 @@
 
      for($x=0;$x<$cantidadtorneos;$x++){
           $torneos.='<div class="contenedor-torneo">
-          <p id="Nombretorneo" >'.$datostorneo[($x)]['nombreTorneo'].'</p> <button id="'.$datostorneo[($x)]['nombreTorneo'].'" onclick="vertorneo()">Ver</button>
+          <p id="Nombretorneo" >'.$datostorneo[($x)]['nombreTorneo'].'</p> <button id="'.$datostorneo[($x)]['nombreTorneo'].'" onclick="verTorneos()">Ver</button>
       </div>';
      }
    
---
+++ Unirsetorneo/infotorneo.php
@@ -0,0 +1,4 @@
+<?php
+    include "../PHP/Servidor/servidor.php";
+    $servidor = new Servidor();
+    echo json_encode($servidor->datosTorneo());
